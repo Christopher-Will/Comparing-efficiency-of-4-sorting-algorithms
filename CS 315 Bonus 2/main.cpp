f23dd769dd3523d6bfd675e6e48ea859b3708cc9
Christopher-Will <c.will186@gmail.com>
2016-06-22 17:30:20 -0400

    Update main.cpp

--- CS 315 Bonus 2/main.cpp
+++ CS 315 Bonus 2/main.cpp
@@ -1,5 +1,4 @@
 //Christopher Will
-// CS315 Bonus 2
 // 4/18/2016
 
 //Program may be executed by pressing F5
@@ -28,7 +27,7 @@ int main()
 			cin.ignore(256, '\n');
 			break;
 		}
-		vec.push_back(input);
+		vec.push_back(input); // add the input to the vector
 		cin >> input;
 	}
 	cout << endl;
@@ -37,7 +36,7 @@ int main()
 	cout << "Please enter the integer value associated with the sorting algorithm you wish to use :" << endl;
 	display();
 	cin >> user_in;
-	if (user_in == 1)
+	if (user_in == 1) // sort using insertion sort
 	{
 		cout << "Vector before sorting " << endl;
 		print(vec);
@@ -46,7 +45,7 @@ int main()
 		print(vec);
 	}
 
-	else if (user_in == 2)
+	else if (user_in == 2) //sort using selection sort
 	{
 		cout << "Vector before sorting " << endl;
 		print(vec);
@@ -54,7 +53,7 @@ int main()
 		cout << "Vector after using Selection Sort" << endl;
 		print(vec);
 	}
-	else if (user_in == 3)
+	else if (user_in == 3) // sort using quicksort
 	{
 		cout << "Vector before sorting " << endl;
 		print(vec);
@@ -62,7 +61,7 @@ int main()
 		cout << "Vector after using Quicksort " << endl;
 		print(vec);
 	}
-	else
+	else // sort using mergesort
 	{
 		cout << "Vector before sorting " << endl;
 		print(vec);
@@ -78,28 +77,28 @@ int main()
 		cout << "************* Comparisons made for n = " << n << " in descending order *************" << endl;
 		for (int i = 0; i < 4; i++) // sort the n # for each of the 4 algorithms
 		{
-			int comparisons = 0;
-			vector<int> vec;
+			int comparisons = 0; // have not made any comparisons in the algorithm yet
+			vector<int> vec; // vector to store the descending numbers
 			for (int k = n; k >= 1; k--)
-				vec.push_back(k);
+				vec.push_back(k); // add the numbers to the vector in descending order
 
-			if (i == 0)
+			if (i == 0) //use insertion sort
 			{
 				insertion_sort(vec, comparisons);
 				cout << "Insertion Sort made " << comparisons << " comparisons" << endl;
 			}
-			else if (i == 1)
+			else if (i == 1) //use selection sort
 			{
 				selection_sort(vec, comparisons);
 				cout << "Selection Sort made " << comparisons << " comparisons" << endl;
 			}
 
-			else if (i == 2)
+			else if (i == 2) //use quicksort
 			{
 				quicksort(vec, 0, vec.size() - 1, comparisons);
 				cout << "Quicksort made " << comparisons << " comparisons" << endl;
 			}
-			else
+			else //use mergesort
 			{
 				merge_sort(vec, 0, vec.size() - 1, comparisons);
 				cout << "Mergesort made " << comparisons << " comparisons" << endl;
@@ -147,44 +146,44 @@ int main()
 
 	n = 100;
 	while (n <= 6400) // add n ints to the vector, shuffle it and keep track of the # of comparisons made. Do this 20 times and
-		// find the avg # of comparisons for each of the 4 sorting algorithms
+			// find the avg # of comparisons for each of the 4 sorting algorithms
 	{
-		for (int i = 1; i <= 4; i++)
+		for (int i = 1; i <= 4; i++) //sort method will depend on the value of i 
 		{
-			vector<int> vec;
+			vector<int> vec; // vector to store the random numbers to be sorted
 			vector<int> compare_vec; // vector to keep track of the comparisons made
 			for (int k = 1; k <= n; k++)
-				vec.push_back(k);
-			for (int j = 1; j <= 20; j++)
+				vec.push_back(k); // add n numbers to the vector in ascending order
+			for (int j = 1; j <= 20; j++) // will randomly shuffle the vector 20 times
 			{
-				srand(time(0));
+				srand(time(0)); //seed the RNG
 				int comparisons = 0;
-				shuffle(vec);
-				if (i == 1)
+				shuffle(vec); //randomly shuffle the vectors contents
+				if (i == 1) //use insertion sort
 				{
 					insertion_sort(vec, comparisons);
 					compare_vec.push_back(comparisons);
 				}
-				else if (i == 2)
+				else if (i == 2) //use selection sort
 				{
 					selection_sort(vec, comparisons);
 					compare_vec.push_back(comparisons);
 				}
-				else if (i == 3)
+				else if (i == 3) //use quicksort
 				{
 					quicksort(vec, 0, vec.size() - 1, comparisons);
 					compare_vec.push_back(comparisons);
 				}
-				else
+				else //use mergesort
 				{
 					merge_sort(vec, 0, vec.size() - 1, comparisons);
 					compare_vec.push_back(comparisons);
 				}
 			}
-			int sum = 0;
+			int sum = 0; //int to hold the number of comparisons made 
 			for (int i = 0; i < 20; i++) // find total # of comparisons for the 20 shuffles
 			{
-				sum += compare_vec[i];
+				sum += compare_vec[i]; //add the # of comparisons made for the ith iteration to the sume
 			}
 			if (i == 1)
 				cout << "For n = " << n << " & using Insertion Sort we had an average of " << sum / 20 << " comparisons" << endl;
@@ -196,7 +195,7 @@ int main()
 				cout << "For n = " << n << " & using Mergesort we had an average of " << sum / 20 << " comparisons" << endl;
 		}
 		cout << endl;
-		n *= 2;
+		n *= 2; //double the value of n until it is > 6400
 	}
 
 	system("pause");
